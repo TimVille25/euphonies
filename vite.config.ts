1c006da2e14b9f70173aed0328e36a6a7746d89e
Timothée Ville <timothee.ville25@gmail.com>
2026-06-01 19:02:49 +0200

    feat: ajouter des styles personnalisés et optimiser la configuration de Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/euphonies/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) =>
+          id.includes('node_modules/react') || id.includes('node_modules/react-dom')
+            ? 'vendor'
+            : undefined,
+      },
+    },
+  },
 });
